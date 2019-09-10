8a3c04ba8871c43503c588986b3da63e35fc5344
Abdulkadir Aba <abdulkadir.aba786@gmail.com>
2019-09-10 07:16:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,7 @@ def introduction (name)
 end 
 
 #introduction_with_language 
+
 def introduction_with_language (name, language)
-  puts "Hi, my name is #{name} and i am learning to program in #{language}."
+  puts "Hi, my name is #{name} and i am learning to program in#{language}."
 end
